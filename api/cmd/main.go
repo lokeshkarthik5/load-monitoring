605a77f94baa5450981a541b43f628417e0e7b5e
lokeshkarthik5 <lokeshkarthic1999@gmail.com>
2026-02-03 13:01:43 +0530

    added server and test init file

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,12 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 
- fmt.Println("Hello World")
+	mux := http.NewServeMux()
+
+	log.Println("The server is running on port 3001")
+
+	srv := http.Server{
+		Addr:    ":" + "3001",
+		Handler: mux,
+	}
+	log.Fatal(srv.ListenAndServe())
 
 }
-
-
-
--- start-monitoring.sh
+++ start-monitoring.sh
@@ -1,3 +1,3 @@
 
-echo "Starting monotoring services!"
+echo "Starting monotoring services! 🚀"
 
