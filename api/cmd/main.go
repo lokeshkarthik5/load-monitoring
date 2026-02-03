5118eabdf49ca48008a19b44013f6b4e0b94e21b
lokeshkarthik5 <lokeshkarthic1999@gmail.com>
2026-02-03 13:26:13 +0530

    server added

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -5,11 +5,17 @@ import (
 	"net/http"
 )
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello World"))
+}
 func main() {
 
 	mux := http.NewServeMux()
 
 	log.Println("The server is running on port 3001")
+
+	mux.HandleFunc("/", rootHandler)
 
 	srv := http.Server{
 		Addr:    ":" + "3001",
--- k6_tests/test-file.js
+++ k6_tests/test-file.js
@@ -1,1 +1,12 @@
 import http from 'k6/http'
+
+import { sleep } from 'k6'
+
+export const options = {
+    iterations: 10
+}
+
+
+export default function() {
+    http.get('http://localhost:3001')
+}
